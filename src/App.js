05cc2c2598420fd0032ffd90648763d01170a672
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-05-14 11:30:07 +0100

    Updated Consumer Dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
-import Wrapper from "./Components/Wrapper";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Wrapper from './Components/Wrapper';
 
-//Pages
-import Home from "./Components/Pages/Home";
-import ConsumerDashboard from './Components/Pages/ConsumerDashboard'
-import EditConsumerProfile from './Components/Pages/EditConsumerProfile'
+// Pages
+import Home from './Components/Pages/Home';
+import ConsumerDashboard from './Components/Pages/ConsumerDashboard';
+import EditConsumerProfile from './Components/Pages/EditConsumerProfile';
+
 function App() {
   return (
     <Router>
-      <Wrapper/>
-        <Route exact={true} path="/" component={Home} />
-        <Route exact ={true} path="/dashboard/consumer" component={ConsumerDashboard} />
+      <Wrapper />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard/consumer" component={ConsumerDashboard} />
         <Route path="/dashboard/consumer/edit" component={EditConsumerProfile} />
-
+      </Switch>
     </Router>
   );
 }
